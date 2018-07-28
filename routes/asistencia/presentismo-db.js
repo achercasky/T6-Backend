14fe16bcbbd6bcaad6aa60932ba7e6ead411089a
aricherca-home <ariel.chercasky@gmail.com>
2018-07-28 14:51:19 -0300

    fix put & post asistencia - se formatea la fecha para el get - post - put

--- routes/asistencia/presentismo-db.js
+++ routes/asistencia/presentismo-db.js
@@ -1,5 +1,6 @@
 'use strict'
 
+var DateFormat = require('dateformat');
 const mongo = require('mongodb');
 const MongoClient = mongo.MongoClient;
 const ObjectID = mongo.ObjectID;
@@ -12,39 +13,105 @@ let DB_COLLECTION_NAME = 'Asistencia';
 
 const HANDLERS = {};
 
-/** POST ASISTENCIA */
-HANDLERS.postAsistenciafromDB = async (request, reply) => {
+/** GET ASISTENCIAS */
+HANDLERS.getAsistenciafromDB = async () => {
 
     const client = await MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
-    var asistencias;
+    var collection;
 
     try {
         const db = await client.db(DB_NAME);
 
-        const body = request.payload;
+        collection = db.collection(DB_COLLECTION_NAME).find().toArray();
 
-        const collection = db.collection(DB_COLLECTION_NAME).findOne({ "materia" : body.materia});
-
-        asistencias = collection
     } catch (err) {
         console.log(err);
     } finally {
         client.close();
     }
 
-    return asistencias.then(function(result) {
-        console.log('postAsistenciafromDB' + result);
+    return collection.then(function(result) {
+        console.log('GET ASISTENCIAS ' + JSON.stringify(result));
+        return result;
+     }).catch(function(error) {
+         console.log('GET ASISTENCIAS ERROR' + error);
+     });
+}
 
-            if(result == null) {
+/** GET ASISTENCIAS BY ID */
+HANDLERS.getAsistenciaByIdfromDB = async (request, h) => {
+
+    const client = await MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+
+    var collection;
+
+    try {
+        const db = await client.db(DB_NAME);
+
+        //var id = new ObjectID(request.params.id);
+
+        var requestDate = request.params.date;
+
+        var date = DateFormat(requestDate, 'shortDate');
+
+        collection = db.collection(DB_COLLECTION_NAME).findOne(
+            {
+                'date': date
+            }
+        );
+
+    } catch (err) {
+        console.log(err);
+    } finally {
+        client.close();
+    }
+
+    return collection.then(function(result) {
+        console.log('GET ASISTENCIAS BY ID' + JSON.stringify(result));
+        return result;
+     }).catch(function(error) {
+         console.log('GET ASISTENCIAS BY ID ERROR' + error);
+     });
+}
+
+/** POST ASISTENCIA */
+HANDLERS.postAsistenciafromDB = async (request, h) => {
+
+    const client = await MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+
+    var collection;
+
+    try {
+        const db = await client.db(DB_NAME);
+
+        const body = request.payload;
+        body.date = DateFormat(body.date, 'shortDate');
+
+        //collection = db.collection(DB_COLLECTION_NAME).findOne({ "materia" : body.materia});
+
+        collection = db.collection(DB_COLLECTION_NAME).insertOne(body);
+    } catch (err) {
+        console.log(err);
+    } finally {
+        client.close();
+    }
+
+    return collection.then(function(result) {
+        console.log('POST ASISTENCIA ' + result);
+        return result;
+
+            /*if(result == null) {
                  return postAsistencia(request, reply);
              }else {
                  return asistencias = {"title": "YA EXISTE!"};
-             }
-     })
+             }*/
+     }).catch(function(error) {
+        console.log('POST ASISTENCIA  ' + error);
+    });
 }
 
-async function postAsistencia(request, reply) {
+/*async function postAsistencia(request, reply) {
     const client = await MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
     var asistencias;
@@ -68,35 +135,84 @@ async function postAsistencia(request, reply) {
 
         return result;
      })  
-}
+}*/
 
-/** Obtener ASISTENCIA by ID*/
-HANDLERS.getAsistenciafromDBById = async (request, h) => {
-
+/** PUT ASISTENCIA */
+HANDLERS.editAsistenciafromDB = async (request, h) => {
     const client = await MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+
+    const horarios = [];
 
     var collection;
 
     try {
         const db = await client.db(DB_NAME);
 
-        var obj_id = new ObjectID(request.params.id);
-
-        console.log(request.params.id);
-
-        collection = db.collection(DB_COLLECTION_NAME).findOne({'_id':obj_id});
+        const body = request.payload;
+        body.date = DateFormat(body.date, 'shortDate');
         
+        horarios.push(new ObjectID(body._id));
+        
+
+        collection = db.collection(DB_COLLECTION_NAME).updateOne( {'_id':{'$in':horarios} }, 
+        {
+            $set: 
+            {
+                "date": body.date, 
+                "materias": body.materias
+            }
+        });
+
     } catch (err) {
         console.log(err);
     } finally {
         client.close();
     }
 
-    return collection.then(function(result) {
-        console.log('PRESENTISMO DB' + result);
+    return collection.then(function (result) {
+        console.log('PUT ALUMNO ' + result);
         return result;
-     })  
+    }).catch(function(error) {
+        console.log('PUT ALUMNO ' + error);
+    }); 
+}
 
+/** BORRAR ASISTENCIA */
+HANDLERS.deleteAsistencia = async (request, h) => {
+
+    const asistencias = [];
+
+    var response;
+
+    const client = await MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+
+    try {
+
+        const db = await client.db(DB_NAME);
+
+        const header = request.payload;
+
+        header.forEach(element => {
+            asistencias.push(new ObjectID(element._id));
+        });
+
+        response = db.collection(DB_COLLECTION_NAME).remove({'_id':{'$in':asistencias}});
+
+    } catch(err) {
+        console.log('Error deleteAsistencia' + err);
+    } finally {
+        client.close();
+    }
+
+    return response.then(function(result) {
+        console.log(result.result.n)
+
+        if(result.result.n == 1) {
+            return "Se Borro el ASISTENCIA";
+        } else {
+            return "No se borro el ASISTENCIA";
+        }
+     })  
 }
 
 module.exports = HANDLERS;
--- routes/asistencia/presentismo.js
+++ routes/asistencia/presentismo.js
@@ -4,11 +4,39 @@ const AsistenciaDB = require('../asistencia/presentismo-db');
 
 const HANDLER = {};
 
+HANDLER.getAsistencia = async (request, reply) => {
+
+    console.log('Paso getAsistencia');
+
+    return AsistenciaDB.getAsistenciafromDB();
+}
+
+HANDLER.getAsistenciaById = async (request, reply) => {
+
+    console.log('Paso getAsistenciaById');
+
+    return AsistenciaDB.getAsistenciaByIdfromDB(request, reply);
+}
+
 HANDLER.sendAsistencia = async (request, reply) => {
 
     console.log('Paso sendAsistencia');
 
     return AsistenciaDB.postAsistenciafromDB(request, reply);
+}
+
+HANDLER.editAsistencia = async (request, reply) => {
+
+    console.log('Paso editAsistencia');
+
+    return AsistenciaDB.editAsistenciafromDB(request, reply);
+}
+
+HANDLER.deleteAsistencia = async (request, reply) => {
+
+    console.log('Paso deleteAsistencia');
+
+    return AsistenciaDB.deleteAsistencia(request, reply);
 }
 
 module.exports = HANDLER;
